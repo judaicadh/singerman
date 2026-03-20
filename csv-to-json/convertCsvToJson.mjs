5fc05a8d4c7a536c4c8171b3b5406f5ce1ddb6c8
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2026-03-20 13:31:33 -0400

    added new entries plus changed Gratz

--- csv-to-json/convertCsvToJson.mjs
+++ csv-to-json/convertCsvToJson.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const csvFilePath = path.join(__dirname, './Singerman-Jan2024 (38).csv');
+const csvFilePath = path.join(__dirname, './Singerman-Jan2024 (39).csv');
 const jsonFilePath = path.join(__dirname, '../src/data/items.json');
 
 // Helpers
